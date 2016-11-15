289edeb28ef3683528127a27389cbc4dee875982
Corentin Noël <corentin@elementary.io>
2016-11-15 13:50:48 +0100

    Ajout du support de la caméra

--- src/projet.py
+++ src/projet.py
@@ -3,11 +3,26 @@ try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+import picamera
+import datetime
+
+camera = picamera.PiCamera()
+camera.resolution = (640, 480)
+
+def stop_record():
+    camera.stop_recording()
+    print(' - Record stoped')
+
+def start_record():
+    camera.start_recording(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'.mp4', format='h264', quality=20)
+    print(' - Record started')
+    GLib.timeout_add_seconds(10, stop_record)
 
 def ir_callback(channel):
     print('This is a edge event callback function!')
     print('Edge detected on channel %s'%channel)
-    print('This is run in a different thread to your main program')
+    if (camera.recording == False):
+        start_record()
 
 def init_gpio_handler():
     channel = 8
