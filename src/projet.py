45a593335c9188a813e23c7cdf63514a55309abf
Corentin Noël <corentin@elementary.io>
2017-01-17 13:58:57 +0100

    Add Dailymotion support to the Python project

--- src/projet.py
+++ src/projet.py
@@ -5,18 +5,33 @@ except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 import picamera
 import datetime
+import dailymotion
+import subprocess
 
 camera = picamera.PiCamera()
-camera.resolution = (640, 480)
+camera.framerate = 25
+d = dailymotion.Dailymotion()
+d.set_grant_type('password', api_key='fc97978e5589a52f86d8', api_secret='537033ac621be2a85d78caa35d273ac1897e8ca7', scope=['userinfo', 'manage_videos'], info={'username': 'tintou@mailoo.org', 'password': 'ferreira'})
 
 def stop_record():
     camera.stop_recording()
     print(' - Record stoped')
+    print(' - Sending video')
+    output_file = current_file.replace (".h264", ".mp4");
+    subprocess.call(["avconv", "-r", "25", "-i", current_file, "-vcodec", "copy", output_file])
+    
+    url = d.upload('./{0}'.format(output_file))
+    d.post('/me/videos', {'url': url, 'title': output_file, 'published': 'false', 'channel': 'news'})
+    print(' - Video sent')
+    return False
 
 def start_record():
-    camera.start_recording(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'.mp4', format='h264', quality=20)
+    global current_file
+    current_file = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'.h264'
+    camera.start_recording(current_file, format='h264', bitrate=17000)
     print(' - Record started')
     GLib.timeout_add_seconds(10, stop_record)
+    return False
 
 def ir_callback(channel):
     print('This is a edge event callback function!')
@@ -32,6 +47,8 @@ def init_gpio_handler():
 
 def main():
     init_gpio_handler()
+    print('logged as ', d.get('/me', {'fields': 'id,fullname'}).get('fullname'))
+    GLib.timeout_add_seconds(10, start_record)
     GLib.MainLoop().run()
     GPIO.cleanup()
 
